b7ebe816e688befcad588a5b62a3b8241eeba829
Leandro <leandrolupano@hotmail.com>
2023-03-13 16:16:50 +0100

    🏗️ Add app, index and store structure files

---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+export type MenuOption = {
+  label: string;
+  path: string;
+};
+// TEMPORAL:
+// const menuOptions: MenuOption[] = [
+//   { label: 'Register', path: '/users/register' },
+//   { label: 'Login', path: '/users/login' },
+//   { label: 'Users', path: '/users' },
+// ];
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
