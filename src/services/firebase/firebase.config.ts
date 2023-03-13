fa1c29ea39dd01cd8b226685324a6281460afe9c
Leandro <leandrolupano@hotmail.com>
2023-03-13 16:17:23 +0100

    🏗️ Add firebase config

---
+++ src/services/firebase/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCCAEEdp1iC-fcQxaQLvBxH_hmogjjvqsw",
+  authDomain: "social-network-challenge.firebaseapp.com",
+  projectId: "social-network-challenge",
+  storageBucket: "social-network-challenge.appspot.com",
+  messagingSenderId: "93609149558",
+  appId: "1:93609149558:web:4795cdcbe7801e20c98b57",
+};
