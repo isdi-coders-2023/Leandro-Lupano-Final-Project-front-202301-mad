b3a348b4f10c5a0404f4870167d22e150d7ec76e
Leandro <leandrolupano@hotmail.com>
2023-03-14 17:59:40 +0100

    🏗️ Add app router

--- src/components/app.router/app.router.tsx
+++ src/components/app.router/app.router.tsx
@@ -1,32 +1,33 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { linkOptions } from './link.options';
+// TEMP. FUTURE ROUTE: import { linkOptions } from './link.options';
 import { NavOption, navMenuOptions } from './nav.menu.options';
 
 // Routes from the navigation menu:
-const Home = lazy(() => import(''));
-const Login = lazy(() => import(''));
-const Products = lazy(() => import(''));
-const MyGuitars = lazy(() => import(''));
-const About = lazy(() => import(''));
-const Users = lazy(() => import(''));
-const Error = lazy(() => import(''));
+const Home = lazy(() => import('../home/home'));
+const Login = lazy(() => import('../home/home'));
+// TEMP. FUTURE ROUTE: const Products = lazy(() => import('../home/home'));
+// TEMP. FUTURE ROUTE:const MyGuitars = lazy(() => import('../home/home'));
+// TEMP. FUTURE ROUTE:const About = lazy(() => import('../home/home'));
+// TEMP. FUTURE ROUTE:const Users = lazy(() => import('../home/home'));
+// TEMP. FUTURE ROUTE:const Error = lazy(() => import('../home/home'));
 
 // Routes from links:
-const GuitarDetails = lazy(() => import(''));
-const EditGuitar = lazy(() => import(''));
-const CreateGuitar = lazy(() => import(''));
-const DeleteGuitar = lazy(() => import(''));
-const EditUser = lazy(() => import(''));
-const DeleteUser = lazy(() => import(''));
+// TEMP. FUTURE ROUTE: const GuitarDetails = lazy(() => import('../home/home'));
+// TEMP. FUTURE ROUTE: const EditGuitar = lazy(() => import('../home/home'));
+// TEMP. FUTURE ROUTE: const CreateGuitar = lazy(() => import('../home/home'));
+// TEMP. FUTURE ROUTE: const DeleteGuitar = lazy(() => import('../home/home'));
+// TEMP. FUTURE ROUTE: const EditUser = lazy(() => import('../home/home'));
+// TEMP. FUTURE ROUTE: const DeleteUser = lazy(() => import('../home/home'));
 
 export function AppRouter() {
   const navMenuOptionsArray: NavOption[] = navMenuOptions;
-  const linkOptionsArray: NavOption[] = linkOptions;
+  // TEMP. FUTURE ROUTE: const linkOptionsArray: NavOption[] = linkOptions;
 
   return (
     <Suspense>
       <Routes>
+        <Route path={'/'} element={<Home></Home>}></Route>
         <Route
           path={navMenuOptionsArray[0].path}
           element={<Home></Home>}
@@ -35,47 +36,47 @@ export function AppRouter() {
           path={navMenuOptionsArray[1].path}
           element={<Login></Login>}
         ></Route>
-        <Route
+        {/* TEMP. FUTURE ROUTE: <Route
           path={navMenuOptionsArray[2].path}
           element={<Products></Products>}
         ></Route>
-        <Route
+        TEMP. FUTURE ROUTE: <Route
           path={navMenuOptionsArray[3].path}
           element={<MyGuitars></MyGuitars>}
         ></Route>
-        <Route
+        TEMP. FUTURE ROUTE: <Route
           path={navMenuOptionsArray[4].path}
           element={<About></About>}
         ></Route>
-        <Route
+        TEMP. FUTURE ROUTE: <Route
           path={navMenuOptionsArray[5].path}
           element={<Users></Users>}
         ></Route>
-        <Route
+        TEMP. FUTURE ROUTE: <Route
           path={linkOptionsArray[0].path}
           element={<GuitarDetails></GuitarDetails>}
         ></Route>
-        <Route
+        TEMP. FUTURE ROUTE: <Route
           path={linkOptionsArray[1].path}
           element={<EditGuitar></EditGuitar>}
         ></Route>
-        <Route
+        TEMP. FUTURE ROUTE: <Route
           path={linkOptionsArray[2].path}
           element={<CreateGuitar></CreateGuitar>}
         ></Route>
-        <Route
+        TEMP. FUTURE ROUTE: <Route
           path={linkOptionsArray[3].path}
           element={<DeleteGuitar></DeleteGuitar>}
         ></Route>
-        <Route
+        TEMP. FUTURE ROUTE: <Route
           path={linkOptionsArray[4].path}
           element={<EditUser></EditUser>}
         ></Route>
-        <Route
+        TEMP. FUTURE ROUTE: <Route
           path={linkOptionsArray[5].path}
           element={<DeleteUser></DeleteUser>}
         ></Route>
-        <Route path={'*'} element={<Error></Error>}></Route>
+        TEMP. FUTURE ROUTE: <Route path={'*'} element={<Error></Error>}></Route> */}
       </Routes>
     </Suspense>
   );
