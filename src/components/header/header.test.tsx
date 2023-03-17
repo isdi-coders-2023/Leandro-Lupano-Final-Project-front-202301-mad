47977882a0cb2424fa045dbde984d050706d914f
Leandro <leandrolupano@hotmail.com>
2023-03-17 17:22:43 +0100

    🏗️ Add header component

---
+++ src/components/header/header.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import Header from './header';
+
+describe('Given Header component', () => {
+  describe('When the component is rendered', () => {
+    test('Then the logo image should be in the document', () => {
+      render(<Header></Header>);
+      const element = screen.getByRole('img');
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
