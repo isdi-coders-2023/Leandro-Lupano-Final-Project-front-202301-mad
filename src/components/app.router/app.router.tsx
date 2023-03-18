03918aedac6fb81c478300c9b2bef8dc5cd8842d
Leandro <leandrolupano@hotmail.com>
2023-03-18 15:54:30 +0100

    🏗️ Add about page, test and update app router

--- src/components/app.router/app.router.tsx
+++ src/components/app.router/app.router.tsx
@@ -1,14 +1,14 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { linkOptions } from './link.options';
-import { NavOption } from './nav.menu.options';
+import { NavOption, navMenuOptions } from './nav.menu.options';
 
 // Routes from the navigation menu:
 const Home = lazy(() => import('../home/home'));
 const Login = lazy(() => import('../login/login'));
 // TEMP. FUTURE ROUTE: const Products = lazy(() => import('../home/home'));
 // TEMP. FUTURE ROUTE:const MyGuitars = lazy(() => import('../home/home'));
-// TEMP. FUTURE ROUTE:const About = lazy(() => import('../home/home'));
+const About = lazy(() => import('../about/about'));
 // TEMP. FUTURE ROUTE:const Users = lazy(() => import('../home/home'));
 // TEMP. FUTURE ROUTE:const Error = lazy(() => import('../home/home'));
 
@@ -21,8 +21,7 @@ const GuitarDetails = lazy(() => import('../home/home'));
 // TEMP. FUTURE ROUTE: const DeleteUser = lazy(() => import('../home/home'));
 
 export function AppRouter() {
-  // TEMPORAL: Hasta tener disponibles los componentes:
-  // const navMenuOptionsArray: NavOption[] = navMenuOptions;
+  const navMenuOptionsArray: NavOption[] = navMenuOptions;
   const linkOptionsArray: NavOption[] = linkOptions;
 
   return (
@@ -38,12 +37,13 @@ export function AppRouter() {
         TEMP. FUTURE ROUTE: <Route
           path={navMenuOptionsArray[1].path}
           element={<MyGuitars></MyGuitars>}
-        ></Route>
-        TEMP. FUTURE ROUTE: <Route
+        ></Route>  */}
+
+        <Route
           path={navMenuOptionsArray[2].path}
           element={<About></About>}
         ></Route>
-        TEMP. FUTURE ROUTE: <Route
+        {/*TEMP. FUTURE ROUTE: <Route
           path={navMenuOptionsArray[3].path}
           element={<Users></Users>}
         ></Route> */}
