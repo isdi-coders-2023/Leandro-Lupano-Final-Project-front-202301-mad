a161ae854aa1aa83811ad87686d4b999d61490ae
Leandro <leandrolupano@hotmail.com>
2023-03-19 13:27:19 +0100

    🏗️ Add read and readId in guitars api repo

---
+++ src/services/repositories/guitars.api.repo.ts
@@ -0,0 +1,60 @@
+import { GuitarServerResponse, GuitarStructure } from '../../models/guitar';
+
+export interface GuitarsRepo<T> {
+  read(token: string, style?: string, page?: string): Promise<T>;
+  readId(token: string, idGuitar: GuitarStructure['id']): Promise<T>;
+}
+
+export class GuitarsApiRepo implements GuitarsRepo<GuitarServerResponse> {
+  url: string;
+
+  constructor() {
+    this.url = 'http://localhost:5000/guitars';
+  }
+
+  async read(
+    token: string,
+    style?: string,
+    page?: string
+  ): Promise<GuitarServerResponse> {
+    if (!style) style = '';
+    if (!page) page = '';
+
+    const url = this.url + '/products?style=' + style + '&page=' + page;
+
+    const resp = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    });
+
+    if (!resp.ok)
+      throw new Error('Error http: ' + resp.status + resp.statusText);
+
+    const guitarsData = (await resp.json()) as GuitarServerResponse;
+
+    return guitarsData;
+  }
+
+  async readId(
+    token: string,
+    idGuitar: GuitarStructure['id']
+  ): Promise<GuitarServerResponse> {
+    const url = this.url + '/details/' + idGuitar;
+
+    const resp = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    });
+
+    if (!resp.ok)
+      throw new Error('Error http: ' + resp.status + resp.statusText);
+
+    const guitarData = (await resp.json()) as GuitarServerResponse;
+
+    return guitarData;
+  }
+}
