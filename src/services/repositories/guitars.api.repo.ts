1fe0f6c0d16b26ed1b24da637ad2bdefcb95c481
Leandro <leandrolupano@hotmail.com>
2023-03-19 13:55:10 +0100

    🏗️ Add update and delete in guitars api repo

--- src/services/repositories/guitars.api.repo.ts
+++ src/services/repositories/guitars.api.repo.ts
@@ -4,6 +4,12 @@ export interface GuitarsRepo<T> {
   read(token: string, style?: string, page?: string): Promise<T>;
   readId(token: string, idGuitar: GuitarStructure['id']): Promise<T>;
   create(token: string, guitarInfo: Partial<GuitarStructure>): Promise<T>;
+  update(
+    token: string,
+    idGuitar: GuitarStructure['id'],
+    guitarInfo: Partial<GuitarStructure>
+  ): Promise<T>;
+  delete(token: string, idGuitar: GuitarStructure['id']): Promise<void>;
 }
 
 export class GuitarsApiRepo implements GuitarsRepo<GuitarServerResponse> {
@@ -79,5 +85,42 @@ export class GuitarsApiRepo implements GuitarsRepo<GuitarServerResponse> {
     const guitarData = (await resp.json()) as GuitarServerResponse;
 
     return guitarData;
+  }
+
+  async update(
+    token: string,
+    idGuitar: GuitarStructure['id'],
+    guitarInfo: Partial<GuitarStructure>
+  ): Promise<GuitarServerResponse> {
+    const url = this.url + '/edit/' + idGuitar;
+
+    const resp = await fetch(url, {
+      method: 'PATCH',
+      body: JSON.stringify(guitarInfo),
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    });
+
+    if (!resp.ok)
+      throw new Error('Error http: ' + resp.status + resp.statusText);
+
+    const guitarData = (await resp.json()) as GuitarServerResponse;
+
+    return guitarData;
+  }
+
+  async delete(token: string, idGuitar: GuitarStructure['id']): Promise<void> {
+    const url = this.url + '/delete/' + idGuitar;
+
+    const resp = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    });
+
+    if (!resp.ok)
+      throw new Error('Error http: ' + resp.status + resp.statusText);
   }
 }
