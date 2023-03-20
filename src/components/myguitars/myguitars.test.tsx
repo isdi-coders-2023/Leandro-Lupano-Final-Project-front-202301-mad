4b24bb5bd0dd6153a7ef7d65da92dde8679318aa
Leandro <leandrolupano@hotmail.com>
2023-03-20 18:22:20 +0100

    ✅ Add guitar card test

--- src/components/myguitars/myguitars.test.tsx
+++ src/components/myguitars/myguitars.test.tsx
@@ -1,14 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter as Router } from 'react-router-dom';
 import MyGuitars from './myguitars';
+import { Provider } from 'react-redux';
+import { store } from '../../store/store';
+
+jest.mock('../guitar.card/guitar.card');
 
 describe('Given the MyGuitars component', () => {
   describe('When the component is rendered', () => {
     test('Then the main title should be in the document', () => {
       render(
-        <Router>
-          <MyGuitars></MyGuitars>
-        </Router>
+        <Provider store={store}>
+          <Router>
+            <MyGuitars></MyGuitars>
+          </Router>
+        </Provider>
       );
       const element = screen.getByRole('heading');
       expect(element).toBeInTheDocument();
