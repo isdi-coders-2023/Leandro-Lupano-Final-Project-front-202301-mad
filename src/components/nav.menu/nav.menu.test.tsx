6e54fa71d0d26547938a9ab237a3423059eae3b4
Leandro <leandrolupano@hotmail.com>
2023-03-24 11:49:47 +0100

    🚧 Add admin role render in nav-menu

--- src/components/nav.menu/nav.menu.test.tsx
+++ src/components/nav.menu/nav.menu.test.tsx
@@ -5,14 +5,30 @@ import { MemoryRouter as Router } from 'react-router-dom';
 import { navMenuOptions } from '../app.router/nav.menu.options';
 import { NavMenu } from './nav.menu';
 import userEvent from '@testing-library/user-event';
+import { useUsers } from '../../hooks/use.users';
+import { store } from '../../store/store';
+import { Provider } from 'react-redux';
+
+jest.mock('../../hooks/use.users');
 
 describe('Given NavMenu component', () => {
-  describe('When the component is rendered', () => {
+  describe('When the component is rendered with token and Admin role', () => {
     beforeEach(() => {
+      (useUsers as jest.Mock).mockReturnValue({
+        usersState: {
+          userLogged: {
+            token: 'testToken',
+            role: 'Admin',
+          },
+        },
+      });
+
       render(
-        <Router>
-          <NavMenu></NavMenu>
-        </Router>
+        <Provider store={store}>
+          <Router>
+            <NavMenu></NavMenu>
+          </Router>
+        </Provider>
       );
     });
 
@@ -27,6 +43,58 @@ describe('Given NavMenu component', () => {
         await userEvent.click(elements[1]);
       });
       expect(elements[1]).toBeInTheDocument();
+    });
+  });
+
+  describe('When the component is rendered without token and User role', () => {
+    beforeEach(() => {
+      (useUsers as jest.Mock).mockReturnValue({
+        usersState: {
+          userLogged: {
+            token: undefined,
+            role: 'User',
+          },
+        },
+      });
+
+      render(
+        <Provider store={store}>
+          <Router>
+            <NavMenu></NavMenu>
+          </Router>
+        </Provider>
+      );
+    });
+
+    test('Then, the login logo should be in the document', () => {
+      const element = screen.getByRole('img');
+      expect(element).toBeInTheDocument();
+    });
+  });
+
+  describe('When the component is rendered with token and User role', () => {
+    beforeEach(() => {
+      (useUsers as jest.Mock).mockReturnValue({
+        usersState: {
+          userLogged: {
+            token: 'testToken',
+            role: 'User',
+          },
+        },
+      });
+
+      render(
+        <Provider store={store}>
+          <Router>
+            <NavMenu></NavMenu>
+          </Router>
+        </Provider>
+      );
+    });
+
+    test('Then, if there is token but the role is User, the login logo should be in the document', () => {
+      const elements = screen.getAllByRole('img');
+      expect(elements[0]).toBeInTheDocument();
     });
   });
 });
--- src/components/nav.menu/nav.menu.tsx
+++ src/components/nav.menu/nav.menu.tsx
@@ -1,9 +1,21 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useUsers } from '../../hooks/use.users';
+import { UsersApiRepo } from '../../services/repositories/users.api.repo';
 import { NavOption, navMenuOptions } from '../app.router/nav.menu.options';
 import style from './nav.menu.style.module.scss';
 
 export function NavMenu() {
+  const userRepo = useMemo(() => new UsersApiRepo(), []);
+
+  const { usersState } = useUsers(userRepo);
+
+  const isLogged: boolean =
+    usersState.userLogged.token !== undefined ? true : false;
+
+  const isAdmin: boolean =
+    usersState.userLogged.role === 'Admin' ? true : false;
+
   const navMenuOptionsArray: NavOption[] = navMenuOptions;
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -22,22 +34,38 @@ export function NavMenu() {
         />
       </Link>
 
-      <img
-        className={style.navMenuBurger}
-        src="./images/burger-menu.png"
-        alt="burger-menu-logo"
-        onClick={handleClick}
-      />
+      {isLogged ? (
+        <>
+          <img
+            className={style.navMenuBurger}
+            src="./images/burger-menu.png"
+            alt="burger-menu-logo"
+            onClick={handleClick}
+          />
 
-      <ul
-        className={isMenuOpen ? style.navMenuOptionsOpen : style.navMenuOptions}
-      >
-        {navMenuOptionsArray.map((option) => (
-          <li key={option.label} onClick={handleClick}>
-            <Link to={option.path}>{option.label}</Link>
-          </li>
-        ))}
-      </ul>
+          <ul
+            className={
+              isMenuOpen ? style.navMenuOptionsOpen : style.navMenuOptions
+            }
+          >
+            {navMenuOptionsArray.map((option) => (
+              <li key={option.label} onClick={handleClick}>
+                <Link to={option.path}>{option.label}</Link>
+              </li>
+            ))}
+
+            {isAdmin ? (
+              <li key="Users" onClick={handleClick}>
+                <Link to="/users">Users</Link>
+              </li>
+            ) : (
+              <></>
+            )}
+          </ul>
+        </>
+      ) : (
+        <div></div>
+      )}
     </nav>
   );
 }
