a4966b2568ece578f017fd0d4048cdb397b49444
Leandro <leandrolupano@hotmail.com>
2023-03-25 21:28:48 +0100

    ✅ Update nav menu test with logout

--- src/components/nav.menu/nav.menu.test.tsx
+++ src/components/nav.menu/nav.menu.test.tsx
@@ -8,13 +8,22 @@ import userEvent from '@testing-library/user-event';
 import { useUsers } from '../../hooks/use.users';
 import { store } from '../../store/store';
 import { Provider } from 'react-redux';
+import { UsersApiRepo } from '../../services/repositories/users.api.repo';
 
 jest.mock('../../hooks/use.users');
+
+const mockNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockNavigate,
+}));
 
 describe('Given NavMenu component', () => {
   describe('When the component is rendered with token and Admin role', () => {
     beforeEach(() => {
       (useUsers as jest.Mock).mockReturnValue({
+        logoutUser: jest.fn(),
         usersState: {
           userLogged: {
             token: 'testToken',
@@ -49,6 +58,7 @@ describe('Given NavMenu component', () => {
   describe('When the component is rendered without token and User role', () => {
     beforeEach(() => {
       (useUsers as jest.Mock).mockReturnValue({
+        logoutUser: jest.fn(),
         usersState: {
           userLogged: {
             token: undefined,
@@ -75,6 +85,7 @@ describe('Given NavMenu component', () => {
   describe('When the component is rendered with token and User role', () => {
     beforeEach(() => {
       (useUsers as jest.Mock).mockReturnValue({
+        logoutUser: jest.fn(),
         usersState: {
           userLogged: {
             token: 'testToken',
@@ -95,6 +106,13 @@ describe('Given NavMenu component', () => {
     test('Then, if there is token but the role is User, the login logo should be in the document', () => {
       const elements = screen.getAllByRole('img');
       expect(elements[0]).toBeInTheDocument();
+    });
+
+    test('Then, if there is token but the role is User, and the logout img is click, the logoutUser function should be called', async () => {
+      const usersMockRepo = {} as unknown as UsersApiRepo;
+      const elements = screen.getAllByRole('img');
+      await userEvent.click(elements[0]);
+      expect(useUsers(usersMockRepo).logoutUser).toHaveBeenCalled();
     });
   });
 });
